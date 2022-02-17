604dc710e59f4e87c5a9cc67141608942bf52934
Paul Rougieux <paul.rougieux@gmail.com>
2022-02-17 14:07:31 +0100

    Implement salvage logging disturbances in priority, before other disturbances

--- scripts/running/run_zz.py
+++ scripts/running/run_zz.py
@@ -24,9 +24,9 @@ Typically you would run this file from a command line like this:
 from libcbm_runner.core.continent import continent
 
 ################################################################################
-combo  = continent.combos['harvest_test']
+combo  = continent.combos['hat']
 runner = combo.runners['ZZ'][-1]
-runner.num_timesteps = 30
+runner.num_timesteps = 22
 runner.run(keep_in_ram=True, verbose=True, interrupt_on_error=True)
 
 # Input events sent to libcbm
