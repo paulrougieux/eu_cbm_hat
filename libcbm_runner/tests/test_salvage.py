0187e3984042d7a1cbc5bafedecd2e29bef51de0
Paul Rougieux <paul.rougieux@gmail.com>
2022-03-03 16:15:23 +0100

    Start to create a test script for py.test

---
+++ libcbm_runner/tests/test_salvage.py
@@ -0,0 +1,44 @@
+"""
+Test salvage logging methods
+
+Execute the test suite from bash with py.test as follows:
+
+    cd ~/repos/libcbm_runner/libcbm_runner
+    py.test
+
+Inspired by the use in pandas
+https://github.com/pandas-dev/pandas/blob/main/pandas/tests/strings/test_find_replace.py
+"""
+
+def test_dist_4_followed_by_29():
+    """Disturbance 4 followed by disturbance 29 salvage logging. Prepare input
+    data for ZZ Trying to overwrite as much as possible of the input data with
+    these data frames """
+    # Overwrite the natural disturbance activity
+    # events, inventory, growth curves and transitions
+
+
+    # Overwrite the events template
+    # reference	For	PA	LU00	H	E	35	con	1	Cur	irw_and_fw	1	FALSE	0	5	0	5
+    # reference	For	PA	LU00	H	E	45	con	1	Cur	irw_and_fw	1	FALSE	0	5	0	5
+
+    # Run the model
+
+
+    # Check output
+
+    # assert [1,2] == [1,2]
+    # assert (np.array([1,2]) == np.array([1,2])).all()
+    # np.testing.assert_allclose(np.array([1,2]),np.array([1,3]))
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/running/run_zz.py
+++ scripts/running/run_zz.py
@@ -26,7 +26,7 @@ from libcbm_runner.core.continent import continent
 ################################################################################
 combo  = continent.combos['hat']
 runner = combo.runners['ZZ'][-1]
-runner.num_timesteps = 22
+runner.num_timesteps = 30
 runner.run(keep_in_ram=True, verbose=True, interrupt_on_error=True)
 
 # Input events sent to libcbm
@@ -34,3 +34,19 @@ events_input = runner.input_data["events"]
 # Events stored in the output
 events_output = runner.output.events
 output_extras = runner.output.extras
+
+
+
+# cols = ['climate', 'con_broad', 'disturbance_type', 'product_created', 'dist_interval_bias']
+# cols += ['irw_vol', 'fw_vol', 'irw_pot', 'fw_pot']
+# #['irw_norm']
+#  df_irw.loc[salv, cols] 
+#  salv = df["last_dist_id"] != -1
+       
+
+# (Pdb) timestep
+# 18
+#     climate  con_broad  disturbance_type product_created dist_interval_bias       irw_vol       fw_vol       irw_pot       fw_pot
+# 32       23         28                26      irw_and_fw                  1  12969.952174  1441.105797  12969.952174  1441.105797
+# 37       25         28                26      irw_and_fw                  1  12967.619034  1440.846559  12967.619034  1440.846559
+
