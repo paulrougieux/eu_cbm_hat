b88ead2768fe5f12587c14434ac409a759664b5c
Viorel Blujdea <Viorel.Blujdea@ec.europa.eu>
2022-07-05 18:05:35 +0200

    adding the 5 potencia scenarios to combos

--- libcbm_runner/combos/__init__.py
+++ libcbm_runner/combos/__init__.py
@@ -20,7 +20,17 @@ from libcbm_runner.combos.special           import Special
 from libcbm_runner.combos.historical        import Historical
 from libcbm_runner.combos.harvest_test      import HarvestTest
 from libcbm_runner.combos.potencia_baseline import Potencia_baseline
+from libcbm_runner.combos.potencia_necp_bme_up100 import Potencia_necp_bme_up100
+from libcbm_runner.combos.potencia_necp_bme_up200 import Potencia_necp_bme_up200
+from libcbm_runner.combos.potencia_necp_bms_down50 import Potencia_necp_bms_down50
+from libcbm_runner.combos.potencia_necp_bms_down90 import Potencia_necp_bms_down90
 
 ###############################################################################
 # List all combo classes to be loaded #
-combo_classes = [Historical, Special, HarvestTest, Hat, Potencia_baseline]
+combo_classes = [Historical, Special, HarvestTest, Hat, 
+                 Potencia_baseline, 
+                 Potencia_necp_bme_up100,
+                 Potencia_necp_bme_up200,
+                 Potencia_necp_bms_down50,
+                 Potencia_necp_bms_down90
+                 ]
