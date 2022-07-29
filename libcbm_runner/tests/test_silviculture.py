a7c496369943a5b56faa57e9324a5b814f8f1ee8
Paul Rougieux <paul.rougieux@gmail.com>
2022-07-29 13:05:18 +0200

    Document test behaviour

--- libcbm_runner/tests/test_silviculture.py
+++ libcbm_runner/tests/test_silviculture.py
@@ -43,21 +43,23 @@ harvest_factors_3 = {'scenario': ['reference', 'reference', 'reference', 'no_ske
 harvest_factors_3 = pandas.DataFrame(harvest_factors_3)
 
 def test_correct_harvest_factors_input():
-    """Test the function that checks harvest_factors input files, with correct input"""
+    """Test the function that checks harvest_factors input files, 
+    with correct input"""
     runner.silv.harvest.raw = harvest_factors_1
     runner.silv.harvest.extra_checks()
 
 def test_incomplete_columns_returns_error():
-    """Test the function that checks harvest_factors input files, with wrong input"""
+    """Test the function that checks harvest_factors input files, 
+    with incomplete classifier column"""
     runner.silv.harvest.raw = harvest_factors_2
     with pytest.raises(ValueError) as excinfo:
         runner.silv.harvest.extra_checks()
     assert "cannot be incomplete" in str(excinfo.value)
 
 def test_skew_factors_do_not_sum_to_one():
-    """Test with a skew factor that do not sum to one"""
+    """Test the function that checks harvest_factors input files
+    with a skew factor that do not sum to one"""
     runner.silv.harvest.raw = harvest_factors_3
     with pytest.raises(ValueError) as excinfo:
         runner.silv.harvest.extra_checks()
     assert "do not sum to one" in str(excinfo.value)
-
