c43c51d1dab4a8b7f579f01307d13a6db98b14a2
Viorel Blujdea <Viorel.Blujdea@ec.europa.eu>
2023-03-30 08:06:06 +0200

    adding one scenarios combination

--- eu_cbm_hat/combos/__init__.py
+++ eu_cbm_hat/combos/__init__.py
@@ -15,31 +15,26 @@ A list of all combo classes.
 # First party modules #
 
 # Internal modules #
-from eu_cbm_hat.combos.reference               import Reference
-from eu_cbm_hat.combos.hat               import Hat
+from eu_cbm_hat.combos.reference         import Reference
+from eu_cbm_hat.combos.no_market_forcing import NoMarketForcing
 from eu_cbm_hat.combos.skewfcth          import Skewfcth
-from eu_cbm_hat.combos.special           import Special
-from eu_cbm_hat.combos.historical        import Historical
-from eu_cbm_hat.combos.harvest_test      import HarvestTest
+from eu_cbm_hat.combos.reference_crf               import Reference_crf
 from eu_cbm_hat.combos.potencia_baseline import Potencia_baseline
 from eu_cbm_hat.combos.potencia_necp_bme_up100 import Potencia_necp_bme_up100
 from eu_cbm_hat.combos.potencia_necp_bme_up200 import Potencia_necp_bme_up200
 from eu_cbm_hat.combos.potencia_necp_bms_down50 import Potencia_necp_bms_down50
 from eu_cbm_hat.combos.potencia_necp_bms_down90 import Potencia_necp_bms_down90
-from eu_cbm_hat.combos.no_market_forcing import NoMarketForcing
+
 
 ###############################################################################
 # List all combo classes to be loaded #
-combo_classes = [Reference, 
-                 Historical,
-                 HarvestTest,
-                 Hat,
+combo_classes = [Reference,
                  NoMarketForcing,
+                 Reference_crf,
+                 Skewfcth,
                  Potencia_baseline,
                  Potencia_necp_bme_up100,
                  Potencia_necp_bme_up200,
                  Potencia_necp_bms_down50,
                  Potencia_necp_bms_down90,
-                 Skewfcth,
-                 Special,
                  ]
