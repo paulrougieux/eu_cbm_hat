1889b36c982c1c668372952fe4a62831e30f03c7
Paul Rougieux <paul.rougieux@gmail.com>
2023-06-26 13:08:47 +0200

    A method to summarise pools

--- eu_cbm_hat/pump/post_processor.py
+++ eu_cbm_hat/pump/post_processor.py
@@ -8,15 +8,8 @@ JRC Biomass Project.
 Unit D1 Bioeconomy.
 """
 
-# Built-in modules #
+from typing import Union, List
 
-# Third party modules #
-
-# First party modules #
-
-# Internal modules #
-
-###############################################################################
 class PostProcessor(object):
     """
     This class will xxxx.
@@ -39,3 +32,34 @@ class PostProcessor(object):
         self.parent.log.info("Post-processing results.")
         # Lorem #
         pass
+
+    def sum_flux_pool(self, by: Union[List[str], str], pools: List[str]):
+        """Aggregate the flux pool table over the "by" variables and for the
+        given list of pools.
+
+        Example
+
+            >>> from eu_cbm_hat.core.continent import continent
+            >>> runner_at = continent.combos["pikbau"].runners["AT"][-1]
+            >>> living_biomass_pools = [
+            >>>     "softwood_merch",
+            >>>     "softwood_other",
+            >>>     "softwood_foliage",
+            >>>     "softwood_coarse_roots",
+            >>>     "softwood_fine_roots",
+            >>>     "hardwood_merch",
+            >>>     "hardwood_foliage",
+            >>>     "hardwood_other",
+            >>>     "hardwood_coarse_roots",
+            >>>     "hardwood_fine_roots",
+            >>> ]
+            >>> runner_at.post_processor.sum_flux_pool(by="year", pools=living_biomass_pools)
+            >>> runner_at.post_processor.sum_flux_pool(by=["year", "forest_type"], pools=living_biomass_pools)
+
+        """
+        df = self.runner.output.pool_flux.groupby(by)[pools].sum()
+        df["combo"] = self.runner.combo.short_name
+        df["iso2_code"] = self.runner.country.iso2_code
+        df["country"] = self.runner.country.country_name
+        df.reset_index(inplace=True)
+        return df
