9afefc1031ddcfe68eb3fe9c7d611efd9ca1d718
Paul Rougieux <paul.rougieux@gmail.com>
2023-07-12 17:44:37 +0200

    Shorten runner creation line

--- scripts/running/run_lu.py
+++ scripts/running/run_lu.py
@@ -17,16 +17,12 @@ Typically you would run this file from a command line like this:
 
 from eu_cbm_hat.core.continent import continent
 
-
 #############################################
 # Declare which scenario combination to run #
 #############################################
-# Scenario combination defined in the yaml file 
+# Scenario combination defined in yaml files
 # `~/repos/eu_cbm_data/combos/harvest_test.yaml`
-# Scenario itself is defined as code in 
-# `~/repos/eu_cbm_hat/eu_cbm_hat/combos/harvest_test.py`
-combo   = continent.combos['hat']
-runner  = combo.runners['LU'][-1]
+runner = continent.combos['pikfair'].runners['LU'][-1]
 runner.num_timesteps = 25
 
 # Run the model
