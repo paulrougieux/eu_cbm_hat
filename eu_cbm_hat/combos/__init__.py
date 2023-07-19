70a8c068dd6ecf1136d6c4440af4ed71386a3b61
Paul Rougieux <paul.rougieux@gmail.com>
2023-07-19 12:25:58 +0200

    Delete unused scenario combinations
    Update simpler instructions with a yaml file only.

--- eu_cbm_hat/combos/__init__.py
+++ eu_cbm_hat/combos/__init__.py
@@ -9,8 +9,17 @@ Unit D1 Bioeconomy.
 
 A list of all combo classes.
 
-Creating a new scenario combination:
+[ Update July 2023] Creating a new scenario combination only requires an
+update to the following yaml file:
 
+    - In `eu_cbm_data/combos` Compile a new yaml file with the combination of
+    scenarios desired. NB. A yaml file is a configuration file that can be
+    edited with a text editor.
+
+[Old instructions before July 2023] Creating a new scenario combination in the
+hard coded way might still be required in case you need a scenario class that
+modifies the behaviour of the scenario combinations beyond what the base runner
+or the dynamic runner can do:
 
 - In `eu_cbm_data`
 
@@ -62,32 +71,12 @@ Creating a new scenario combination:
 
 # Internal modules #
 from eu_cbm_hat.combos.reference         import Reference
-from eu_cbm_hat.combos.no_market_forcing import NoMarketForcing
-from eu_cbm_hat.combos.skewfcth          import Skewfcth
-from eu_cbm_hat.combos.reference_crf              import Reference_crf
-# from eu_cbm_hat.combos.half_harvest               import Half_harvest
-# from eu_cbm_hat.combos.plus_20_harvest            import Plus_20_harvest
-from eu_cbm_hat.combos.potencia_baseline import Potencia_baseline
-from eu_cbm_hat.combos.potencia_necp_bme_up100 import Potencia_necp_bme_up100
-from eu_cbm_hat.combos.potencia_necp_bme_up200 import Potencia_necp_bme_up200
-from eu_cbm_hat.combos.potencia_necp_bms_down50 import Potencia_necp_bms_down50
-from eu_cbm_hat.combos.potencia_necp_bms_down90 import Potencia_necp_bms_down90
 from eu_cbm_hat.combos.ia_2040 import IA_2040
 
 from eu_cbm_hat import eu_cbm_data_pathlib
 
 # Hard coded combo classes
 combo_classes = [Reference,
-                 NoMarketForcing,
-                 Reference_crf,
-                 Skewfcth,
-                 Potencia_baseline,
-                 #                  Half_harvest,
-                 #                  Plus_20_harvest,
-                 Potencia_necp_bme_up100,
-                 Potencia_necp_bme_up200,
-                 Potencia_necp_bms_down50,
-                 Potencia_necp_bms_down90,
                  IA_2040,
                 ]
 combo_classes_dict = dict(zip([s.short_name for s in combo_classes], combo_classes))
