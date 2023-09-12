9a42d0e3280ed3d575505f8a4a249899b9350a26
Paul Rougieux <paul.rougieux@gmail.com>
2023-09-12 21:09:40 +0200

    Combo post processing script at the command line
    Takes a list of combo names as an argument

---
+++ scripts/post_processing/process_scenario_combo.py
@@ -0,0 +1,29 @@
+""" Post processing of the EU_CBM_HAT scenario combinations output
+
+See post_processor/agg_combos.py for documentation of the save_agg_combo_output
+
+Usage:
+
+    cd $HOME/repos/eu_cbm/eu_cbm_hat/scripts/post_processing
+    # or on BDAP
+    cd $HOME/eu_cbm/eu_cbm_hat/scripts/post_processing
+    ipython -i process_scenario_combo.py -- --combo_names reference
+    ipython -i process_scenario_combo.py -- --combo_names reference pikbau pikfair
+
+"""
+
+import argparse
+from eu_cbm_hat.post_processor.agg_combos import save_agg_combo_output
+
+parser = argparse.ArgumentParser(
+    description="Post processing of the EU_CBM_HAT scenario combinations"
+)
+parser.add_argument(
+    "--combo_names", type=str, help="List of names of scenario combos to post process"
+)
+
+shell_args = parser.parse_args()
+COMBO_NAMES = shell_args.combo_names
+
+for x in COMBO_NAMES:
+    save_agg_combo_output(x)
