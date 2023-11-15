6a25aa1acd8691f8c1cc0dd7d2b2e52bad28a477
Paul Rougieux <paul.rougieux@gmail.com>
2023-11-15 16:45:01 +0100

    Add country code in the error message

--- eu_cbm_hat/post_processor/area.py
+++ eu_cbm_hat/post_processor/area.py
@@ -18,7 +18,7 @@ def apply_to_all_countries(data_func, combo_name, **kwargs):
         except FileNotFoundError as e_file:
             print(e_file)
         except ValueError as e_value:
-            print(e_value)
+            print(key, e_value)
     df_all.reset_index(inplace=True, drop=True)
     return df_all
 
