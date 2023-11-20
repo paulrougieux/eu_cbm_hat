c6c6648c5ee93954071b65134bb49e468606348d
Paul Rougieux <paul.rougieux@gmail.com>
2023-11-20 13:46:56 +0100

    Aggregate area and area change

--- eu_cbm_hat/post_processor/area.py
+++ eu_cbm_hat/post_processor/area.py
@@ -4,69 +4,120 @@ from typing import List, Union
 class Area:
     """Compute the area changes through time and across classifiers
 
-
-    Investigate issues with area changes
+    Usage
 
         >>> from eu_cbm_hat.core.continent import continent
         >>> import matplotlib.pyplot as plt
         >>> runner = continent.combos['reference'].runners['LU'][-1]
-        >>> area = runner.post_processor.area.df
+        >>> df = runner.post_processor.area.df
 
     Total area stays constant through time
 
-        >>> total_area = area.groupby("year")["area"].sum()
+        >>> total_area = df.groupby("year")["area"].sum()
         >>> total_area.round().unique()
 
     The status changes through time
 
-        >>> area_st = area.groupby(["year", "status"])["area"].sum().reset_index()
-        >>> area_st = area_st.pivot(columns="status", index="year", values="area")
-        >>> area_st.plot()
+        >>> area_cols = df.columns[df.columns.str.contains("area")].to_list()
+        >>> area_st = df.groupby(["year", "status"])[area_cols].sum().reset_index()
+        >>> area_st_wide = area_st.pivot(columns="status", index="year", values="area")
+        >>> area_st_wide.plot()
         >>> plt.show()
 
-    Why is it needed to group by classifiers first.
+    Is the area stable or changing across each of the other classifiers?
+
+        >>> cols = runner.post_processor.classifiers.columns.to_list()
+        >>> cols = [x for x in cols if x not in ["identifier", "year"]]
+        >>> for cl in cols:
+        >>>     print(cl)
+        >>>     df_cl = df.groupby([cl, "year"])["area"].sum().reset_index()
+        >>>     print(df_cl["area"].round().unique())
+
+    Area grouped by classifiers
+
+        >>> df_agg_cl = runner.post_processor.area.df_agg_by_classifiers
+
+    Group area by status and check if the diff in area is explained by afforestation and deforestation
+
+        >>> area_st = runner.post_processor.area.df_agg(["year", "status"])
+        >>> area_st["area_diff"] = area_st["area"] - area_st["area_tm1"]
+
+    Investigate issues with area changes
+
+        >>> index = ["year", "status", "last_disturbance_type", "time_since_land_class_change", "land_class"]
+        >>> cols = ["area", "area_deforested_current_year", "area_afforested_current_year"]
+        >>> df3 = df.query("year in [2021, 2022, 2023]").groupby(index)[cols].agg("sum")
+
+    Why do we need to group by classifiers first?
 
         >>> index = runner.post_processor.classifiers.columns.to_list()
         >>> index.remove("identifier")
         >>> df.value_counts(index, sort=False)
-    
+
     At the end of the simulation a given set of classifiers can be repeated a
     thousand times with different values of time since last disturbance, last
     disturbance type, age class etc.
 
-    At the classifier level
+    Group by region, climate and status
 
-
-    At the year and status level
+        >>> area_reclst = df.groupby(runner.post_processor.sink.groupby_sink)["area"].sum().reset_index()
 
     """
 
     def __init__(self, parent):
         self.parent = parent
         self.runner = parent.runner
-        self.combo_name = self.runner.combo.short_name
-        self.pools = self.parent.pools
-        self.fluxes = self.parent.fluxes
-        self.classifiers = self.parent.classifiers
 
     @cached_property
     def df(self):
         """Area  at the most level of details available"""
-        # TODO: remove pools columns, keep only status columns
-        df = self.pools
+        df = self.parent.pools
+        selected_cols = ['identifier',
+                         'timestep',
+                         'year',
+                         'status',
+                         'forest_type',
+                         'region',
+                         'mgmt_type',
+                         'mgmt_strategy',
+                         'climate',
+                         'con_broad',
+                         'site_index',
+                         'growth_period',
+                         'last_disturbance_type',
+                         'time_since_last_disturbance',
+                         'time_since_land_class_change',
+                         'growth_enabled',
+                         'enabled',
+                         'land_class',
+                         'age',
+                         'growth_multiplier',
+                         'regeneration_delay']
+        selected_cols += df.columns[df.columns.str.contains("area")].to_list()
         return df
 
     @cached_property
     def df_agg_by_classifiers(self):
-        """Area t and area t-1 at the classifier level"""
-        df = self.df
-        index = self.classifiers.columns.to_list()
-        # TODO: group by classifiers columns first
-        time_columns = ["identifier", "year", "timestep"]
-        index = [col for col in index if col not in time_columns]
-        df.sort_values(["year"] + index, inplace=True)
-        df["area_tm1"] = df.groupby(index)["area"].transform(lambda x: x.shift())
+        """Area t at the classifier level"""
+        index = self.parent.classifiers.columns.to_list()
+        index.remove("identifier")
+        area_columns = self.df.columns[self.df.columns.str.contains("area")].to_list()
+        df_agg = self.df.groupby(index)[area_columns].agg("sum").reset_index()
+        return df_agg
 
     def df_agg(self, groupby: Union[List[str], str] = None):
-        """Area aggregated by the given grouping variables"""
+        """Area aggregated by the given grouping variables and area t-1  """
+        if isinstance(groupby, str):
+            groupby = [groupby]
+        time_columns = ["identifier", "year", "timestep"]
+        df = self.df_agg_by_classifiers
+        # Aggregate by the given groupby variables
+        area_columns = df.columns[df.columns.str.contains("area")].to_list()
+        df_agg = df.groupby(groupby)[area_columns].agg("sum").reset_index()
+        # Index to compute the area at t-1
+        index = [col for col in groupby if col not in time_columns]
+        # Arrange by group variable with year last to prepare for shift()
+        df_agg.sort_values(index + ["year"], inplace=True)
+        df_agg["area_tm1"] = df_agg.groupby(index)["area"].transform(lambda x: x.shift())
+        return df_agg
 
