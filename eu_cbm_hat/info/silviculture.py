b851e5705c5831dbd98f210a9702bcdc4781fc6c
blujdvi <86364720+blujdvi@users.noreply.github.com>
2025-06-13 18:14:26 +0200

    Update silviculture.py

--- eu_cbm_hat/info/silviculture.py
+++ eu_cbm_hat/info/silviculture.py
@@ -370,9 +370,9 @@ class EventsTemplates(BaseSilvInfo):
     @property
     def dup_cols(self):
         return (
-        self.cols 
-            + ["sw_start", "sw_end", "hw_start", "hw_end"]
-            + ["last_dist_id", "scenario"]
+        list(self.country.orig_data.classif_names.values())
+            + ["scenario", "sw_start", "sw_end", "hw_start", "hw_end"]
+            + ["last_dist_id"]
         )
 
     def extra_checks(self):
