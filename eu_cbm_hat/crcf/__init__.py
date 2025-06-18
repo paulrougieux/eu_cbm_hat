fae59c2dc5060dcdc2989a2753f8aeb39e5644fa
Matteo Piccardo <matteo.pcc@gmail.com>
2025-06-18 11:30:01 +0200

    added country simulator code

--- eu_cbm_hat/crcf/__init__.py
+++ eu_cbm_hat/crcf/__init__.py
@@ -4,3 +4,5 @@ A subpackage that reuses some of the EU-CBM-HAT classes to build a dedicated
 libcbm model runner.
 
 """
+from eu_cbm_hat.crcf.runner import Runner
+from eu_cbm_hat.crcf.country_simulator import CountrySimulator
