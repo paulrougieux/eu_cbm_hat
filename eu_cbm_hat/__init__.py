b9fa2bb6d7b1c9b1d4c2658812f67286bb8195d3
Paul Rougieux <paul.rougieux@gmail.com>
2025-07-04 16:12:23 +0200

    Move constants to avoid circular imports #115

--- eu_cbm_hat/__init__.py
+++ eu_cbm_hat/__init__.py
@@ -16,48 +16,39 @@ feedback under [issue
 
 """
 
-# Special variables #
+# Special variables
 __version__ = '2.0.1'
 
-# Built-in modules #
-import os
-import sys
-import pathlib
+# Import constants first (no circular dependency)
+from eu_cbm_hat.constants import (
+    project_name,
+    project_url,
+    CARBON_FRACTION_OF_BIOMASS,
+    module_dir,
+    module_dir_pathlib,
+    repos_dir,
+    git_repo,
+    eu_cbm_data_dir,
+    eu_cbm_data_pathlib,
+    eu_cbm_aidb_dir,
+    eu_cbm_aidb_pathlib,
+)
 
-# First party modules #
-from autopaths import Path
-from autopaths.dir_path import DirectoryPath
-from plumbing.git import GitRepo
+# Now import classes (after constants are available)
+from eu_cbm_hat.bud import Bud
 
-# Constants #
-project_name = 'eu_cbm_hat'
-project_url  = 'https://gitlab.com/bioeconomy/eu_cbm/eu_cbm_hat'
-CARBON_FRACTION_OF_BIOMASS = 0.49
+# Use __all__ to let type checkers know what is part of the public API.
+__all__ = [
+    "Bud",
+    "eu_cbm_aidb_dir",
+    "eu_cbm_aidb_pathlib",
+    "eu_cbm_data_dir",
+    "eu_cbm_data_pathlib",
+    "repos_dir",
+    "module_dir",
+    "module_dir_pathlib",
+    "CARBON_FRACTION_OF_BIOMASS",
+    "project_name",
+    "project_url",
+]
 
-# Get paths to module #
-self       = sys.modules[__name__]
-module_dir = Path(os.path.dirname(self.__file__))
-module_dir_pathlib = pathlib.Path(os.path.dirname(self.__file__))
-
-# The repository directory #
-repos_dir = module_dir.directory
-
-# The module is maybe in a git repository #
-git_repo = GitRepo(repos_dir, empty=True)
-
-# Where is the data, default case #
-eu_cbm_data_dir = DirectoryPath("~/eu_cbm/eu_cbm_data/")
-
-# But you can override that with an environment variable #
-if os.environ.get("EU_CBM_DATA"):
-    eu_cbm_data_dir = DirectoryPath(os.environ['EU_CBM_DATA'])
-
-# Prepare the move to pathlib
-eu_cbm_data_pathlib = pathlib.Path(str(eu_cbm_data_dir))
-
-# Where are the AIDBs, default case
-eu_cbm_aidb_dir = DirectoryPath("~/eu_cbm/eu_cbm_aidb/")
-
-# But you can override that with an environment variable #
-if os.environ.get("EU_CBM_AIDB"):
-    eu_cbm_aidb_dir = DirectoryPath(os.environ['EU_CBM_AIDB'])
