f71badb0fd9c4a4faae82298a78833d4f695ed0e
Paul Rougieux <paul.rougieux@gmail.com>
2025-07-16 11:52:53 +0200

    Change the age class length from 10 to 1 in adapt_age_class

--- eu_cbm_hat/bud/adapt_age_classes.py
+++ eu_cbm_hat/bud/adapt_age_classes.py
@@ -2,8 +2,8 @@
 
 import pandas as pd
 
-def adapt_age_classes(growth_curves):
-    """Adapt age_classes to the max age given in the growth curve
+def adapt_age_classes(growth_curves, age_class_length):
+    """Adapt age_classes to the max age given in the growth curve and to the age class length
 
     Reduce the length of the age_classes.csv file according to the number of
     columns supplied in the growth_curves.csv in order to avoid this error:
@@ -16,13 +16,18 @@ def adapt_age_classes(growth_curves):
         >>> import pandas as pd
         >>> from eu_cbm_hat.bud.adapt_age_classes import adapt_age_classes
         >>> growth_curves = pd.DataFrame({"vol0" : [0], "vol1": [30], "vol2": [90]})
-        >>> adapt_age_classes(growth_curves)
+        >>> adapt_age_classes(growth_curves, 10)
+        >>> adapt_age_classes(growth_curves, 1)
 
     """
+    if age_class_length not in [1, 10]:
+        msg = "Age class length can only take a value of 1 or 10."
+        msg += f"\nCurrently age_class_length: {age_class_length}"
+        raise ValueError(msg)
     vol_cols = growth_curves.columns[growth_curves.columns.str.contains("^vol")].to_list()
     df = pd.DataFrame({"vol_col" : vol_cols})
     df["age_class_id"] = df["vol_col"].str.replace("vol", "AGEID")
-    df["size"] = 10
+    df["size"] = age_class_length
     df.loc[0, "size"] = 0
     df.drop(columns={"vol_col"}, inplace=True)
     return df
