915d3b6e14857b3381c8bb432c8f8e043ff04db7
Paul Rougieux <paul.rougieux@gmail.com>
2025-07-18 10:43:18 +0200

    Describe the model and add publications in the documentation

--- eu_cbm_hat/__init__.py
+++ eu_cbm_hat/__init__.py
@@ -10,7 +10,30 @@ Unit D1 Bioeconomy.
 
 # Introduction
 
-There are 3 ways to run the Carbon Budget Model with eu_cbm_hat:
+This documentation describes EU-CBM-HAT an adaptation of the Carbon Budget
+Model (CBM) for EU countries, with the addition of a Harvest Allocation Tool
+(HAT). `eu_cbm_hat` is a python package that enables the assessment of forest
+CO2 emissions and removals under scenarios of forest management, natural
+disturbances and forest-related land use changes. For more information on the
+structure of the model and the research questions that have been analysed see
+the section on related publication below.
+
+EU-CBM-HAT depends on a software library called
+[libcbm](https://github.com/cat-cfs/libcbm_py). libcbm was developed and is
+maintained by the Forest carbon accounting team of Natural Resources Canada.
+The `eu_cbm_hat` package was developed on top of this library by the Joint
+Research Centre (JRC) of the European Commission to add capabilities such
+as: scenario processing, a data structure for EU counties, a harvest allocation
+tool, growth modification and estimates of the harvested wood products sink.
+eu_cbm_hat is publicly available on the python package index at
+[https://pypi.org/project/eu-cbm-hat](https://pypi.org/project/eu-cbm-hat). The
+source code is available openly on
+[https://gitlab.com/bioeconomy/eu_cbm/eu_cbm_hat](https://gitlab.com/bioeconomy/eu_cbm/eu_cbm_hat)
+and the documentation you are currently reading is available at
+[https://bioeconomy.gitlab.io/eu_cbm/eu_cbm_hat/eu_cbm_hat.html](https://bioeconomy.gitlab.io/eu_cbm/eu_cbm_hat/eu_cbm_hat.html).
+
+
+There are 3 ways to run the model:
 
 1. Using a [runner](eu_cbm_hat/core/runner.html) created from simulation tools
   in the [core](eu_cbm_hat/core.html) directory. This runner has been developed
@@ -110,6 +133,63 @@ The data is processed by sub modules:
 
 Runner made for simulations of Carbon Removal and Carbon Farming. This type of
 object is not documented yet. Look at the source code in the crcf directory.
+
+
+# Related Publications
+
+In chronological order.
+
+- Kurz WA, Dymond CC, White TM, Stinson G, Shaw CH, Rampley GJ, Smyth C,
+  Simpson BN, Neilson ET, Trofymow JA, Metsaranta J. **CBM-CFS3: a model of
+  carbon-dynamics in forestry and land-use change implementing IPCC standards**.
+  Ecological modelling. 2009 Feb 24;220(4):480-504.
+  https://doi.org/10.1016/j.ecolmodel.2008.10.018
+
+  - This is the main publication describing the Carbon Budget Model structure.
+    It is a previous version called CBM-CFS3.
+
+
+- Roberto Pilli, Giacomo Grassi, Werner A. Kurz, Carolyn E. Smyth, Viorel
+  Blujdea, **Application of the CBM-CFS3 model to estimate Italy's forest carbon
+  budget, 1995–2020,** Ecological Modelling, Volume 266, 2013, Pages 144-171,
+  ISSN 0304-3800, https://doi.org/10.1016/j.ecolmodel.2013.07.007.
+
+    - This paper used a previous version called CBM-CFS3. Figure 3 in the paper
+      explains the need to switch from an init (historical) growth period to a
+      current growth period. The
+      [Simulation.switch_period](/eu_cbm_hat/cbm/simulation.html#Simulation.switch_period)
+      method changes the growth period from "Init" to "Cur". As a result all
+      stands will have the value "Cur" for the growth_period classifier from
+      inventory start year onwards. Note the inventory start year is stored in
+      `runner.country.inventory_start_year` and defined in
+      [Country.set_years](eu_cbm_hat/core/country.html#Country.set_years).
+
+- Pilli, Roberto, Blujdea, Viorel N.B., Rougieux, Paul, Grassi, Giacomo,
+  Mubareka, Sarah Betoul, 2024 Volume, increment, and aboveground biomass data
+  series and biomass conversion and expansion factors for the main forest types
+  of EU Member States Annals of Forest Science This collection reports the
+  standing stock volume, increment, aboveground biomass, and biomass conversion
+  and expansion factors attributed to 222 forest types and 48 different
+  management types, representative of 25 EU Member States. Dataset
+  https://zenodo.org/records/11387301. Article
+  https://doi.org/10.1186/s13595-024-01256-5
+
+- Blujdea, V., Rougieux, P., Sinclair, L., Morken, S., Pilli, R., Grassi, G.,
+  Mubareka, S. and Kurz, W., **The JRC Forest Carbon Model: description of
+  EU-CBM-HAT**, EUR 31299 EN, Publications Office of the European Union,
+  Luxembourg, 2022, ISBN 978-92-76-58867-2, doi:10.2760/244051, JRC130609.
+  https://publications.jrc.ec.europa.eu/repository/handle/JRC130609
+
+- Pilli, R., Blujdea, V.N., Rougieux, P., Grassi, G. and Mubareka, S.B., **The
+  calibration of the JRC EU Forest Carbon Model within the historical period 2010 - 2020**,
+  Publications Office of the European Union, Luxembourg, 2024,
+  doi:10.2760/222407, JRC135639. https://dx.doi.org/10.2760/222407
+
+- Rougieux, P., Pilli, R., Blujdea, V., Mansuy, N. and Mubareka, S.B.,
+  **Simulating future wood consumption and the impacts on Europe`s forest sink to
+  2070**, Publications Office of the European Union, Luxembourg, 2024,
+  doi:10.2760/17191, JRC136526.
+  https://publications.jrc.ec.europa.eu/repository/handle/JRC136526
 
 
 """
