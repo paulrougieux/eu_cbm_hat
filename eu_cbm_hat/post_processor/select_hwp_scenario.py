320eda88b6524be4d4c154db72a6931eb2b08095
blujdvi <86364720+blujdvi@users.noreply.github.com>
2025-08-14 16:21:14 +0200

    add TODO_2, suggesting to add more arguments to  def select_hwp_scenario

--- eu_cbm_hat/post_processor/select_hwp_scenario.py
+++ eu_cbm_hat/post_processor/select_hwp_scenario.py
@@ -1,6 +1,16 @@
 """Functions to select HWP scenario objects part of the post processor
 
-TODO: Define how to add "recycling" and "substitution" arguments to the select_hwp_scenario method.
+TODO_1: Define how to add "recycling" and "substitution" arguments to the select_hwp_scenario method.
+
+TODO_2: IF POSSIBLE: Thse are hardcoded and maybe we should pass them as arguments to the select_hwp_scenario method. 
+        Define how to add following arguments to the select_hwp_scenario method:  
+        
+        # # Number of common years to be used to compute the fractions of semifinished to logs
+        self.n_years_dom_frac = 3
+        # Add recycling information or not
+        self.add_recycling = True
+        # Set export import factors to one
+        self.no_export_no_import = False
 
 - Currently the "recycling" scenario changes the  recycled_wood_factor and
   recycled_paper_factor based on the same scenario column called
@@ -15,8 +25,8 @@ from eu_cbm_hat.core.continent import continent
 
 def select_hwp_scenario(
     iso2_code,
-    combo="reference",
-    hwp_frac="default"):
+    forest_mngm_scenario="reference",
+    hwp_frac_scenario="default"):
     """Select a scenario combination, HWP scenario, recycling and susbstitution
     scenario n and return a post_processor.hwp object.
 
@@ -36,14 +46,14 @@ def select_hwp_scenario(
 
     """
 
-    if combo is None:
-        combo="reference",
-    if hwp_frac is None:
-        hwp_frac ="default"
+    if forest_mngm_scenario is None:
+        forest_mngm_scenario="reference",
+    if hwp_frac_scenario is None:
+        hwp_frac_scenario ="default"
     # Select the post processor HWP object
-    hwp = continent.combos[combo].runners[iso2_code][-1].post_processor.hwp
+    hwp = continent.combos[forest_mngm_scenario].runners[iso2_code][-1].post_processor.hwp
     # Define properties
-    hwp.hwp_frac_scenario = hwp_frac
+    hwp.hwp_frac_scenario = hwp_frac_scenario
     return hwp
 
 
@@ -62,7 +72,7 @@ def stock_sink_results(**kwargs):
     """
     hwp = select_hwp_scenario(**kwargs)
     df = hwp.stock_sink_results
-    df["combo"] = hwp.runner.combo.short_name
+    df["forest_mngm_scenario"] = hwp.runner.combo.short_name
     # Place the last column first
     cols = df.columns.to_list()
     cols = cols[-1:] + cols[:-1]
