f4839bb3645902deed35df26441b06beb0631ea3
Paul Rougieux <paul.rougieux@gmail.com>
2025-12-02 18:19:32 +0100

    Get unique values from irw_frac_by_dist config and complete share_eu_cbm_data function
    Signed-off-by: Paul Rougieux <paul.rougieux@gmail.com>

--- scripts/comparison/share_eu_cbm_data_with_forest_navigator.py
+++ scripts/comparison/share_eu_cbm_data_with_forest_navigator.py
@@ -16,7 +16,7 @@ directory.
 We only share the common files that are in the list in this script. For the
 given list of countries.
 
-See development notes under issue: 
+See development notes under issue:
 
     no_management
 
@@ -24,14 +24,17 @@ See development notes under issue:
 
 from pathlib import Path
 from typing import Union, List
-import pandas as pd
 import shutil
+import pandas as pd
 
 from eu_cbm_hat import eu_cbm_data_pathlib
 from eu_cbm_hat.core.continent import continent
+
+
 # Change these input parameters
 DEST_DIR = Path("~/eu_cbm/eu_cbm_data_forest_navigator").expanduser()
 COMMON_FILES_TO_SHARE = ["country_codes.csv", "reference_years.csv"]
+
 
 def share_one_csv_file(
     orig_dir: Path,
@@ -84,86 +87,73 @@ def share_one_csv_file(
     df.to_csv(dest_csv_file, index=False)
 
 
-def share_country_data(combo_names, country_codes):
-    """Share country-specific data for a given scenario, filtering CSV files.
-
-    This function searches for all CSV files in the country's data directory,
-    filters rows where 'scenario' column matches the given scenario_combo,
-    and saves the filtered data to the destination directory, preserving
-    the subdirectory structure.
+def share_eu_cbm_data(scenario_combo, country_codes, dest_dir):
+    """Share EU CBM DATA for a given list of scenarios and a selection of countries
 
     Parameters
-    country_code : st
-        The ISO country code (e.g., 'IT') for which to share data.
+    ----------
+    scenario_combo : str or list of str
+        The scenario combo name(s), e.g., "reference"
+    country_codes : list of str
+        List of ISO country codes (e.g., ['IT', 'IE', 'CZ'])
+    dest_dir : str or Path
+        The destination directory path
 
     Examples
     --------
-    cd /home/paul/rp/eu_cbm/eu_cbm_hat/scripts/comparison
-    ipython
-    from share_eu_cbm_data_with_forest_navigator import share_country_data
-    share_country_data(combo_names = ["reference", "no_management"], 
-                       country_codes = ["CZ", "IE", "IT"])
-    # this_combo = "reference"
-    # this_country = "IT"
+    share_eu_cbm_data(scenario_combo="reference",
+                      country_codes=["IT", "IE", "CZ"],
+                      dest_dir="~/eu_cbm/eu_cbm_data_navigator")
     """
-    if isinstance(combo_names, str):
-        combo_names = [combo_names]
+    if isinstance(scenario_combo, str):
+        combo_names = [scenario_combo]
+    else:
+        combo_names = scenario_combo
     if isinstance(country_codes, str):
         country_codes = [country_codes]
-    country_orig_dir = eu_cbm_data_pathlib / "countries" / country_code
-    country_dest_dir = DEST_DIR / "countries" / country_code
-    # Load scenarios combo yaml files. Create a dictionary containing many
-    # configuration dictionaries for each scenario combination yaml input file.
+    dest_dir = Path(dest_dir).expanduser()
+
+    # Load scenarios combo yaml files
     combo_configs = {name: continent.combos[name].config for name in combo_names}
+
+    # Share common data
+    share_common_data(COMMON_FILES_TO_SHARE, dest_dir)
+
+    # Share country data
     for this_country in country_codes:
-        # Silviculture
-        # Which scenarios are in the combo config?
-        for this_combo, config in combo_configs.items():
+        country_orig_dir = eu_cbm_data_pathlib / "countries" / this_country
+        country_dest_dir = dest_dir / "countries" / this_country
 
-            config["irw_frac_by_dist"].values()
+        # Collect all unique scenarios from all combos
+        all_scenarios = set()
+        for config in combo_configs.values():
+            all_scenarios.update(config["irw_frac_by_dist"].values())
 
+        scenarios = list(all_scenarios)
 
+        # Find all CSV files in the country directory
+        csv_files = list(country_orig_dir.glob("**/*.csv"))
 
-        share_one_csv_file(orig_dir=eu_cbm_data_pathlib / "countries/IT",
-                           relative_path="silv/irw_frac_by_dist.csv",
-                           dest_dir="~/eu_cbm/eu_cbm_data_forest_navigator/countries/IT",
-                           scenarios=["reference", "no_management"])
-        # Activities
-
-
-    # Copy files
-    relative_path = orig_path.relative_to(country_orig_dir)
-
-    # Activities
-
-    share_one_csv_file(orig_dir=eu_cbm_data_pathlib / "countries/IT",
-                       relative_path="silv/irw_frac_by_dist.csv",
-                       dest_dir="~/eu_cbm/eu_cbm_data_forest_navigator/countries/IT",
-                       scenarios=["reference", "no_management"])
-
-    # For each combo config value create a relative path to the corresponding
-    # csv file and associate a scenario name
-
-    # For each csv file associate a scenario based on the values in the
-    # scenario combination yaml file
-    csv_files = [p for p in country_orig_dir.glob("**/*.csv")]
-
-    # move the file for inventory management
-    activity
-    scenario = combo_config["inventory"]["mgmt"]
-    orig_file = country_orig_dir / "mgmt" / "inventory.csv"
+        for orig_csv_file in csv_files:
+            relative_path = orig_csv_file.relative_to(country_orig_dir)
+            share_one_csv_file(
+                orig_dir=country_orig_dir,
+                relative_path=str(relative_path),
+                dest_dir=country_dest_dir,
+                scenarios=scenarios
+            )
 
 
 
-def share_common_data(country_codes, files_to_share):
+def share_common_data(files_to_share, dest_dir):
     """Share common input files for the given list of countries
 
     Parameters
     ----------
-    country_codes : list of str
-        List of ISO country codes (not used in this implementation).
     files_to_share : list of str
         List of common file names to copy.
+    dest_dir : Path
+        The destination directory path
 
     Examples
     --------
@@ -173,7 +163,7 @@ def share_common_data(country_codes, files_to_share):
     share_common_data()
     """
     common_orig_dir = eu_cbm_data_pathlib / "common"
-    common_dest_dir = DEST_DIR / "common"
+    common_dest_dir = dest_dir / "common"
 
     for file_name in files_to_share:
         src_path = common_orig_dir / file_name
