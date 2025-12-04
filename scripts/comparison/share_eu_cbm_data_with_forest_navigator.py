692e47ae399256b78e4325a9b2cc7e2f9988a105
Paul Rougieux <paul.rougieux@gmail.com>
2025-12-04 10:04:04 +0100

    Correct the loop copying files based on a scenario mapping to file names

--- scripts/comparison/share_eu_cbm_data_with_forest_navigator.py
+++ scripts/comparison/share_eu_cbm_data_with_forest_navigator.py
@@ -3,15 +3,33 @@
 - Selected scenarios: reference
 - Selected countries: IT, IE, CZ
 
-Create a python function that shares only the data for the given scenario yaml
-file and country name. The function takes a scenario combo and shares all lines
-in all input files that correspond to that scenario. For a given country. It
+A function that shares only the data for the given list of scenario
+combinations and country names. For a given scenario combination combo yaml
+file the function takes a scenario combo and shares all lines in all input
+files that correspond to that scenario. For the selected countries country. It
 saves the output files with the same data structure inside a destination
 directory.
 
+Usage as a command line script:
+
+    ipython -i 
+
+Usage from python 
+
+    cd ~/eu_cbm/eu_cbm_hat/scripts/comparison
+    ipython
+
+    from share_eu_cbm_data_with_forest_navigator import share_one_csv_file
+    from share_eu_cbm_data_with_forest_navigator import get_scenarios_from_combos
+    from share_eu_cbm_data_with_forest_navigator import share_eu_cbm_data
+    from share_eu_cbm_data_with_forest_navigator import share_common_data
+
+    # Share country data
     share_eu_cbm_data(scenario_combo="reference",
                       country_codes=["IT", "IE", "CZ"],
                       dest_dir="~/eu_cbm/eu_cbm_data_navigator")
+    # Share common data
+    share_common_data(COMMON_FILES_TO_SHARE, dest_dir)
 
 We only share the common files that are in the list in this script. For the
 given list of countries.
@@ -26,15 +44,12 @@ from pathlib import Path
 from typing import Union, List
 import shutil
 import pandas as pd
-
 from eu_cbm_hat import eu_cbm_data_pathlib
 from eu_cbm_hat.core.continent import continent
-
 
 # Change these input parameters
 DEST_DIR = Path("~/eu_cbm/eu_cbm_data_forest_navigator").expanduser()
 COMMON_FILES_TO_SHARE = ["country_codes.csv", "reference_years.csv"]
-
 
 def share_one_csv_file(
     orig_dir: Path,
@@ -80,19 +95,49 @@ def share_one_csv_file(
     try:
         df = pd.read_csv(orig_csv_file)
     except pd.errors.EmptyDataError:
+        print(f"No data in {orig_csv_file} not copying.")
         return
-    if 'scenario' in df.columns:
+    if 'scenario' in df.columns and scenarios:
         df = df[df['scenario'].isin(scenarios)]
+    if "scenario" in df.columns and scenarios is None:
+        msg = "There is a scenario column in the file: "
+        msg += f"{orig_csv_file}\n"
+        msg += "Please define which scenarios to select."
+        msg += f"Current value of scenarios: {scenarios}"
+        raise ValueError(msg)
     dest_csv_file.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(dest_csv_file, index=False)
 
 
-def share_eu_cbm_data(scenario_combo, country_codes, dest_dir):
+def get_scenarios_from_combos(combo_names):
+    """Collect unique scenarios for each file type from the given combo names.
+
+    Examples
+    --------
+     get_scenarios_from_combos(combo_names=["reference", "no_management"])
+    """
+    combo_configs = {name: continent.combos[name].config for name in combo_names}
+    activities_files = ['inventory', 'events', 'growth_curves', 'transitions']
+    other_files = ['events_templates', 'irw_frac_by_dist', 'harvest_factors', 'harvest']
+    selected_scenarios_in_files = {}
+    for file_name in activities_files + other_files:
+        all_scenarios = set()
+        for config in combo_configs.values():
+            try:
+                all_scenarios.update(config[file_name].values())
+            except AttributeError:
+                all_scenarios.update([config[file_name]])
+        scenarios = list(all_scenarios)
+        selected_scenarios_in_files[file_name] = scenarios
+    return selected_scenarios_in_files
+
+
+def share_eu_cbm_data(combo_names, country_codes, dest_dir):
     """Share EU CBM DATA for a given list of scenarios and a selection of countries
 
     Parameters
     ----------
-    scenario_combo : str or list of str
+    combo_names : str or list of str
         The scenario combo name(s), e.g., "reference"
     country_codes : list of str
         List of ISO country codes (e.g., ['IT', 'IE', 'CZ'])
@@ -101,66 +146,43 @@ def share_eu_cbm_data(scenario_combo, country_codes, dest_dir):
 
     Examples
     --------
-    share_eu_cbm_data(scenario_combo="reference",
+    share_eu_cbm_data(combo_names=["reference", "no_management"],
                       country_codes=["IT", "IE", "CZ"],
                       dest_dir="~/eu_cbm/eu_cbm_data_navigator")
     """
-    if isinstance(scenario_combo, str):
-        combo_names = [scenario_combo]
+    if isinstance(combo_names, str):
+        combo_names = [combo_names]
     else:
-        combo_names = scenario_combo
+        combo_names = combo_names
     if isinstance(country_codes, str):
         country_codes = [country_codes]
     dest_dir = Path(dest_dir).expanduser()
-
-    # Load scenarios combo yaml files
-    combo_configs = {name: continent.combos[name].config for name in combo_names}
-    # file_name = "irw_frac_by_dist"
-    activities_files = ['inventory', 'events', 'growth_curves', 'transitions']
-    other_files = ['events_templates', 'irw_frac_by_dist', 'harvest_factors', 'harvest']
-    selected_scenarios_in_files =  {}
-    for file_name in activities_files + other_files:
-        # print(combo_configs["reference"][file_name])
-        # Collect all unique scenarios from all combos
-        all_scenarios = set() # To get unique values
-        for config in combo_configs.values():
-            try: # Multi year case
-                all_scenarios.update(config[file_name].values())
-            except AttributeError: # single year case
-                # 'str' object has no attribute 'values'
-                all_scenarios.update([config[file_name]])
-        scenarios = list(all_scenarios)
-        print("\n\n\n", file_name, ":", scenarios)
-        print(config[file_name])
-        selected_scenarios_in_files[file_name] = scenarios
-
-    # Share common data
-    share_common_data(COMMON_FILES_TO_SHARE, dest_dir)
-
+    scenario_files_map = get_scenarios_from_combos(combo_names)
     # Share country data
     for this_country in country_codes:
+        print("\n\n")
         country_orig_dir = eu_cbm_data_pathlib / "countries" / this_country
         country_dest_dir = dest_dir / "countries" / this_country
-
-        # Activities: 'inventory', 'events', 'growth_curves', 'transitions'
-        # Do nothing 'climate_adjustment',
-        # Other files
-        # Activities
-
-
         # Find all CSV files in the country directory
         csv_files = list(country_orig_dir.glob("**/*.csv"))
-
         for orig_csv_file in csv_files:
             relative_path = orig_csv_file.relative_to(country_orig_dir)
+            # Ignore some files not defined in the reference yaml file
+            if orig_csv_file.stem in ["disturbance_matrix_value", "events_templates_original"]:
+                print(f"Ignoring {orig_csv_file}")
+                continue 
+            if orig_csv_file.stem in scenario_files_map.keys():
+                scenarios = scenario_files_map[orig_csv_file.stem]
+            else:
+                scenarios = None
+            print(orig_csv_file)
+            print("Selected scenarios: {scenarios}\n")
             share_one_csv_file(
                 orig_dir=country_orig_dir,
-                relative_path=str(relative_path),
+                relative_path=relative_path,
                 dest_dir=country_dest_dir,
                 scenarios=scenarios
             )
-
-
 
 def share_common_data(files_to_share, dest_dir):
     """Share common input files for the given list of countries
@@ -187,3 +209,6 @@ def share_common_data(files_to_share, dest_dir):
         dest_path = common_dest_dir / file_name
         dest_path.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy2(src_path, dest_path)
+
+
+
